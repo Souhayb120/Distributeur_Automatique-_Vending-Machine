1d28c7894f24994cf288423e6dbc4662cc04a5a0
Samira12-dev <samiraelboussidi663@gmail.com>
2025-11-07 17:46:23 +0100

    add update stock

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static class VendingMachine {
+        static ArrayList<String> produits = new ArrayList<>(Arrays.asList("Eau", "Soda", "Chips", "Chocolat"));
+        static ArrayList<Double> prix = new ArrayList<>(Arrays.asList(5.0, 8.0, 12.0, 15.0));
+        static ArrayList<Integer> stock = new ArrayList<>(Arrays.asList(10, 5, 7, 3));
+    }
+    // stock
+    static void updateStock(int produitNum , double montant) {
+        double prixProduit = VendingMachine.prix.get(produitNum);
+        int StockActuel = VendingMachine.stock.get(produitNum);
+        if(StockActuel > 0){
+            if(montant < prixProduit){
+                System.out.println("The amount is unsifficient, the prix is "+prixProduit +"MAD");
+                return;
+            }
+            else if(montant >= prixProduit){
+                double restMoney = montant - prixProduit;
+                VendingMachine.stock.set(produitNum ,StockActuel -1);
+                System.out.println("you have Bought : " + VendingMachine.produits.get(produitNum));
+                System.out.println("Monnaie rendue :"+restMoney +"MAD");
+                System.out.println("Quantity Stock :"+VendingMachine.stock.get(StockActuel));
+            }
+        }else{
+            System.out.println("Stock epuise pour "+ VendingMachine.produits.get(produitNum));
         }
+    }
+    //Acheter Produit
+    public static void  AcheterPrduit(){
+        //afficherPrduits();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre product number :");
+        int choixproduit = sc.nextInt() -1;
+ if(choixproduit >= 0 && choixproduit < VendingMachine.produits.size()){
+    System.out.println("Entrez le montant insere :");
+    double montant = sc.nextDouble();
+    updateStock(choixproduit, montant);
+    
+ }else{
+    System.out.println("Produit invalid !");
+ }
+    }
+
+    public static void main(String[] args) {
+   
     }
 }
