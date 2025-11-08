f0294698c77b01866ba4e609321495599d07ca87
souhayb <samihadi120@gmail.com>
2025-11-08 22:19:58 +0100

    improvment step

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,16 @@ public class Main {
         static ArrayList<String> produits = new ArrayList<>(Arrays.asList("Eau", "Soda", "Chips", "Chocolat"));
         static ArrayList<Double> prix = new ArrayList<>(Arrays.asList(5.0, 8.0, 12.0, 15.0));
         static ArrayList<Integer> stock = new ArrayList<>(Arrays.asList(10, 5, 7, 3));
+         static int countAchat = 0;
     }
+   
+  
     // stock
     static void updateStock(int produitNum , double montant) {
         double prixProduit = VendingMachine.prix.get(produitNum);
         int StockActuel = VendingMachine.stock.get(produitNum);
-        if(StockActuel > 0){
+        try{
+             if(StockActuel > 0){
             if(montant < prixProduit){
                 System.out.println("The amount is unsifficient, the prix is "+prixProduit +"MAD");
                 return;
@@ -23,28 +27,39 @@ public class Main {
                 System.out.println("you have Bought : " + VendingMachine.produits.get(produitNum));
                 System.out.println("Monnaie rendue :"+restMoney +"MAD");
                 System.out.println("Quantity Stock :"+VendingMachine.stock.get(StockActuel));
+                VendingMachine.countAchat += 1;
+                System.out.println("Total Achats :"+ VendingMachine.countAchat);
             }
         }else{
             System.out.println("Stock epuise pour "+ VendingMachine.produits.get(produitNum));
         }
+        }
+        catch(Exception e){
+            System.out.println("Error :"+ e.getMessage());
+        }
+       
     }
     //Acheter Produit
     public static void  AcheterPrduit(){
-        //afficherPrduits();
-        Scanner sc = new Scanner(System.in);
+        try{
+         Scanner sc = new Scanner(System.in);
         System.out.println("Entre product number :");
         int choixproduit = sc.nextInt() -1;
- if(choixproduit >= 0 && choixproduit < VendingMachine.produits.size()){
-    System.out.println("Entrez le montant insere :");
-    double montant = sc.nextDouble();
-    updateStock(choixproduit, montant);
+        if(choixproduit >= 0 && choixproduit < VendingMachine.produits.size()){
+         System.out.println("Entrez le montant insere :");
+        double montant = sc.nextDouble();
+        updateStock(choixproduit, montant);
     
  }else{
     System.out.println("Produit invalid !");
  }
+        }
+        catch(Exception e){
+            System.out.println("Error :"+ e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-   
+        AcheterPrduit();
     }
 }
